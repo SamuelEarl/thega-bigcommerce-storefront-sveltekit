75baf3ec972dcf5a767e923f188d61fd855a66e0
Samuel Earl <samueltremain@gmail.com>
2025-10-31 11:08:23 -0700

    Working on policy pages

--- src/routes/(storefront)/policies/returns-exchanges-refunds/+page.svelte
+++ src/routes/(storefront)/policies/returns-exchanges-refunds/+page.svelte
@@ -8,40 +8,57 @@
 
 <h2>What's your return and exchange policy?</h2>
 
-<p>We don't offer returns and exchanges unless there is something wrong with your order. If that is the case, then please let us know by <Link href="/contact-us">contacting us</Link> within 15 days of the delivery date or the estimated delivery date. We're not able to accept reports filed after this timeframe.</p>
+<p>If there is something wrong with your order, then please see the <Link href="#problem-report">Submitting a problem report</Link> section below.</p>
 
-<p>Any claims for misprinted/damaged/defective items must be submitted within 15 days after the product has been received. For packages lost in transit, all claims must be submitted no later than 15 days after the estimated delivery date. Claims deemed an error on our part are covered at our expense.</p>
+<p>However, since our products are made specifically for you when you place an order, we are unable to offer returns and exchanges for any orders that do not meet the criteria for submitting a problem report.</p>
+
+<h2>I received misprinted/damaged/defective products, I received the wrong order, or my order is past the delivery date</h2>
 
 <p>Please see the <Link href="#problem-report">Submitting a problem report</Link> section below.</p>
 
 <h2>Do you offer refunds?</h2>
 
-<p>Refunds are only offered to customers that receive the wrong items or damaged items. If any of these apply, please <Link href="/contact-us">contact us</Link> with photos of wrong/damaged items and we'll sort that out for you.</p>
-
-<p>Please see the <Link href="#problem-report">Submitting a problem report</Link> section below.</p>
+<p>Since our products are made specifically for you when you place an order, refunds are only offered to customers that receive the wrong items or damaged items. If any of these apply, please see the <Link href="#problem-report">Submitting a problem report</Link> section below.</p>
 
 <h2>Can I exchange an item for a different size/color?</h2>
 
-<p>At this time, we don't offer exchanges. If you're unsure which size would fit better, check out our sizing charts—we have one for every item listed on our store, in the product description section. Though rare, it's possible that an item you ordered was mislabeled. If that's the case, please <Link href="/contact-us">contact us</Link> within a week after receiving your order. Include your order number and photos of the mislabeled item, and we'll send you a new one, or issue a refund!</p>
+<p>At this time, we don't offer exchanges. If you're unsure which size would fit better, check out our sizing charts—we have one for every item listed on our store, in the product description section. Though rare, it's possible that an item you ordered was mislabeled. If that's the case, please see the <Link href="#problem-report">Submitting a problem report</Link> section below.</p>
 
-<p>Please see the <Link href="#problem-report">Submitting a problem report</Link> section below.</p>
-
-<!-- Bookmark Link -->
-<div id="problem-report-container">
+<div class="bookmark-link-container">
   <div id="problem-report"></div>
 </div>
 
 <h2>Submitting a problem report</h2>
 
-<p>If you notice an issue with your order, please submit a problem report. You can also submit a problem report if more than five business days have passed after the order's estimated delivery date and you still haven't received the order.</p>
+<p>We're so sorry if any of the following situations applies to you:</p>
 
-<p>Make sure to submit your problem report within 15 days of the delivery date or the estimated delivery date. We're not able to accept reports filed after this timeframe.</p>
+<ul>
+  <li>The order you received had misprinted/damaged/defective products.</li>
+  <li>You received the wrong order.</li>
+  <li>Your package was lost in transit (i.e. more than five business days have passed since the estimated delivery date and you still have not received your order).</li>
+</ul>
+
+<p>If any of these situations applies to you, then please follow the steps below to submit a problem report within 15 days of the delivery date or the estimated delivery date. We're not able to accept reports filed after this timeframe.</p>
+
+<p>If your package was lost in transit, then please go to our <Link href="/policies/shipping-and-delivery-policies">Shipping &amp; Delivery Policies</Link> page and follow the guidelines under the section titled <Link href="/policies/shipping-and-delivery-policies#lost-in-transit">My order might be lost in transit. What should I do?</Link> before submitting a problem report.</p>
+
+<p>Claims deemed an error on our part are covered at our expense.</p>
+  
+<p>To help us resolve this for you quickly, please follow these steps:</p>
 
 <ol>
   <li>Go to our <Link href="/contact-us">Contact Us</Link> page and use either method to contact us.</li>
   <li>Enter <strong>Problem Report</strong> as the subject line.</li>
-  <li>Describe the issue in detail and, if relevant, upload photos. To avoid delays, please include clear images with your initial report. For faster resolution, ensure your photos follow our guidelines. See the heading "Photo guidelines when submitting a problem report" below.</li>
+  <li>Describe the issue in detail.</li>
+  <li>Include your order number and any other details you may have about your order.</li>
+  <li>If relevant, upload photos. To avoid delays, please include clear images with your initial report. For faster resolution, ensure your photos follow our guidelines. See the <Link href="#photo-guidelines">Photo guidelines when submitting a problem report</Link> section below.</li>
 </ol>
+
+<p>We'll get back to you with a resolution as soon as possible!</p>
+
+<div class="bookmark-link-container">
+  <div id="photo-guidelines"></div>
+</div>
 
 <h2>Photo guidelines when submitting a problem report</h2>
 
@@ -79,10 +96,10 @@
 
 <style>
   @media (--xs-up) {
-    #problem-report-container {
+    .bookmark-link-container {
       position: relative;
 
-      & #problem-report {
+      & #problem-report, & #photo-guidelines {
         position: absolute;
         top: -80px;
       }
--- src/routes/(storefront)/policies/shipping-and-delivery-policies/+page.svelte
+++ src/routes/(storefront)/policies/shipping-and-delivery-policies/+page.svelte
@@ -4,9 +4,13 @@
 
 <h1>Shipping & Delivery Policies</h1>
 
+<h2>How do I track my order?</h2>
+
+<p>You'll receive a tracking link via email when your order ships out. If you have any questions about your tracking or shipment, then <Link href="/contact-us">contact us</Link>.</p>
+
 <h2>When will I get my order?</h2>
 
-<p>Usually, it takes 3–7 days to fulfill an order, after which it’s shipped out. The shipping time depends on your location, but can be estimated as follows:</p>
+<p>Usually, it takes 3–7 days to fulfill an order, after which it's shipped out. The shipping time depends on your location, but can be estimated as follows:</p>
 
 <ul>
   <li>USA: 3–4 business days</li>
@@ -16,17 +20,17 @@
   <li>International: 10–20 business days</li>
 </ul>
 
-<h2>Where will my order ship from?</h2>
-
-<p>We work with an on-demand order fulfillment company with facilities worldwide!</p>
-
 <h2>Will I be charged customs for my order?</h2>
 
 <p>An additional customs and tax fee can occur on international orders. This fee is not in our control and is assessed by your local customs office. Customs policies vary widely for every country so please check with your local customs office directly to see if they apply duties and taxes to your purchases.</p>
 
-<h2>My order should be here by now, but I still don't have it. What should I do?</h2>
+<div class="bookmark-link-container">
+  <div id="lost-in-transit"></div>
+</div>
 
-<p>Before getting in touch with us, please help us out by doing the following:</p>
+<h2>My order might be lost in transit. What should I do?</h2>
+
+<p>Before submitting a problem report, please help us out by doing the following:</p>
 
 <ul>
   <li>Check your shipping confirmation email for any mistakes in the delivery address</li>
@@ -34,7 +38,7 @@
   <li>Stop by your neighbors in case the courier left the package with them</li>
 </ul>
 
-<p>If the shipping address was correct, and the package wasn't left at the post office or at your neighbor’s, <Link href="/contact-us">contact us</Link> with us with your order number.</p>
+<p>If the shipping address was correct, and the package wasn't left at the post office or at your neighbor's, then <Link href="/policies/returns-exchanges-refunds#problem-report">submit a problem report</Link> and we will get is sorted out as quickly as possible.</p>
 
 <p>If you did find a mistake in your delivery address, we can send you a replacement order, but shipping will be at your own cost.</p>
 
@@ -45,3 +49,16 @@
 <h2>Unclaimed Shipments</h2>
 
 <p>Shipments that go unclaimed are returned to our facility and you will be responsible for the cost of a reshipment to yourself (if and as applicable).</p>
+
+<style>
+  @media (--xs-up) {
+    .bookmark-link-container {
+      position: relative;
+
+       & #lost-in-transit {
+        position: absolute;
+        top: -80px;
+      }
+    }
+  }
+</style>
